4dec92c69d345be2082760c1518ce5d9f68f27b1
anaszammit <azammit.ieu2023@student.ie.edu>
2025-01-31 11:24:55 +0100

    simple_string

---
+++ simple_string.py
@@ -0,0 +1,16 @@
+s= "Hello"
+print(s)
+s2='Hello sir i47q08r784'
+print(s2)
+s=('And she said: "Hello!"')
+print(s)
+print(",.\"\"\"")
+s="hello"
+print(s[2],s[4])
+print(s[-1])
+print(s[1],s[-5])
+
+
+
+
+
