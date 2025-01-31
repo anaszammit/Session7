8e398838d32f373b9a91e615d57b77d2b6abc3e6
anaszammit <azammit.ieu2023@student.ie.edu>
2025-01-31 12:07:46 +0100

    simple_string

---
+++ operation_string.py
@@ -0,0 +1,25 @@
+s1= "hello"
+s2="world"
+print(s1 + " "+ s2)
+print(s1, "hello")
+print(3*s2)
+print((10//2)*s1)
+print(s1, len(s1))
+print(3*s2, len(3*s2))
+for c in s2:
+    print(c*4, end="")
+
+print()
+new_string= ""
+for c in s2:
+     new_string+= c*4
+     print(new_string)
+
+
+     print("h" in s1)
+     print("d" in s2)
+     print("x" in s1)
+     print("wor" in s2)
+
+
+
